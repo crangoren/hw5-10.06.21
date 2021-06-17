919357ccab5b272dc2bdeb36e0ccd9e339529133
crangoren <crangoren@gmail.com>
2021-06-17 12:30:43 +0300

    added homework!

--- src/Lessons/Main.java
+++ src/Lessons/Main.java
@@ -8,50 +8,36 @@ public class Main {
 
     public static final int CARS_COUNT = 4;
     public static final int HALF_CARS_COUNT = CARS_COUNT/2;
-    //final int THREADS_COUNT = CARS_COUNT;
-    //final CountDownLatch cdCars = new CountDownLatch(THREADS_COUNT);
+
 
     public static void main(String[] args) throws InterruptedException {
 
 
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
 
+        CyclicBarrier cb = new CyclicBarrier(5);
+        CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
 
-        CyclicBarrier load = new CyclicBarrier(5);
-        CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+        for (int i = 0; i < cars.length; i++){
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb, cdl);}
 
 
-                for (int i = 0; i < cars.length; i++)
-
-                    cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
-
-
-        for (Car car : cars) {
-            try {
-                new Thread(car).start();
-                load.await();
-            }catch (BrokenBarrierException | InterruptedException e) {
-                e.printStackTrace();
-            }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
         }
-
-
-
-
+        try {
+            cb.await();
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cb.await();
 
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-
-//        ready.start();
-//        startRace.start();
-//        startRace.join();
-//        thread2.start();
-//        thread3.start();
-//        thread2.join();
-//        thread3.join();
 
     }
 }
